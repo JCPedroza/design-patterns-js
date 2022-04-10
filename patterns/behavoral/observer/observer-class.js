b7c0e02f95c60a7ed6c5c90f09f0e2319521283f
JCPedroza <nemesis4go10@hotmail.com>
2022-04-10 05:38:28 -0500

    refa (observer): use class inheritance

--- patterns/behavoral/observer/observer-class.js
+++ patterns/behavoral/observer/observer-class.js
@@ -3,42 +3,88 @@
  */
 class Subject {
   constructor () {
-    this.observers = new Set();
     this.notifications = 0;
+    this._observers = new Set();
   }
 
   /**
    * Add observer to the observers collection.
-   * @param {Observer} obs Observer to add.
+   * @param {Object} obs Observer to add.
+   * @returns {Object} Reference to self.
    */
   attach (obs) {
-    this.observers.add(obs);
+    this._observers.add(obs);
+    return this;
   }
 
   /**
    * Remove observer from the observers collection.
-   * @param {Observer} obs Observer to remove.
+   * @param {Object} obs Observer to remove.
+   * @returns {Object} Reference to self.
    */
   detach (obs) {
-    this.observers.delete(obs);
+    this._observers.delete(obs);
+    return this;
+  }
+
+  /**
+   * Get the number of current observers.
+   * @returns {number} Size of the observers collection.
+   */
+  get aggregateCount () {
+    return this._observers.size;
   }
 
   /**
    * Notify all observers.
    */
-  notify () {
-    for (const obs of this.observers) {
-      obs.update();
+  _notify (state) {
+    for (const obs of this._observers) {
+      obs.update(state);
       this.notifications++;
     }
   }
 }
 
 /**
+ * Represents a switch with a boolean state.
+ */
+class Switch extends Subject {
+  constructor () {
+    super();
+    this._switch = false;
+  }
+
+  /**
+   * Negates the switch value.
+   * @returns {Object} Reference to self.
+   */
+  toggleSwitch () {
+    this._switch = !this._switch;
+    this._notify(this.switch);
+    return this;
+  }
+
+  /**
+   * Get switch value.
+   * @returns {boolean} True if switch is on, false otherwise.
+   */
+  get switch () {
+    return this._switch;
+  }
+}
+
+/**
+ * Factory for Switch.
+ * @returns {Object} A new switch instance.
+ */
+const createSwitch = () => new Switch();
+
+/**
  * Attaches to subjects that control the execution of the update method.
  */
 class Observer {
-  constructor (name) {
+  constructor () {
     this.updates = 0;
   }
 
@@ -50,8 +96,41 @@ class Observer {
   }
 }
 
+/**
+ * Represents a light emitting bulb.
+ */
+class Lightbulb extends Observer {
+  constructor () {
+    super();
+    this._isOn = false;
+  }
+
+  /**
+   * Turn on / off depending on the switche's state.
+   * @param {boolean} state Current subject state.
+   */
+  update (state) {
+    super.update();
+    this._isOn = state;
+  }
+
+  /**
+   * Get isOn value.
+   * @returns {boolean} True if bulb is on, false otherwise.
+   */
+  get isOn () {
+    return this._isOn;
+  }
+}
+
+/**
+ * Factory for Lightbulb.
+ * @returns {Object} A new Lightbulb instance.
+ */
+const createLightbulb = () => new Lightbulb();
+
 module.exports = {
-  Subject,
-  Observer,
-  id: 'subject class with observers set'
+  createSwitch,
+  createLightbulb,
+  id: 'subject class with observers set and class inheritance'
 };
--- patterns/behavoral/observer/observer-class.spec.js
+++ patterns/behavoral/observer/observer-class.spec.js
@@ -1,34 +1,51 @@
 const { expect } = require('chai');
 
-const { Subject, Observer, id } = require('./observer-class');
+const { createSwitch, createLightbulb, id } = require('./observer-class');
 
 describe(`Observer pattern "${id}"`, () => {
-  const subject = new Subject();
-  const observerA = new Observer();
-  const observerB = new Observer();
+  const subject = createSwitch();
+  const observerA = createLightbulb();
+  const observerB = createLightbulb();
+
+  // Validate state of all involved actors.
+  const checkState = (swtch, notif, aggre, isOnA, updtA, isOnB, updtB) => {
+    expect(subject.switch).to.equal(swtch);
+    expect(subject.notifications).to.equal(notif);
+    expect(subject.aggregateCount).to.equal(aggre);
+
+    expect(observerA.isOn).to.equal(isOnA);
+    expect(observerA.updates).to.equal(updtA);
+
+    expect(observerB.isOn).to.equal(isOnB);
+    expect(observerB.updates).to.equal(updtB);
+  };
 
   it('lets observers attach to subjects', () => {
     subject.attach(observerA);
-    subject.attach(observerB);
-
-    expect(subject.observers.size).to.equal(2);
+    checkState(false, 0, 1, false, 0, false, 0);
   });
 
   it('lets subjects notify observers', () => {
-    subject.notify();
+    subject.toggleSwitch();
+    checkState(true, 1, 1, true, 1, false, 0);
+  });
 
-    expect(subject.notifications).to.equal(2);
-    expect(observerA.updates).to.equal(1);
-    expect(observerB.updates).to.equal(1);
+  it('manages multiple observers', () => {
+    subject.attach(observerB);
+    subject.toggleSwitch();
+    checkState(false, 3, 2, false, 2, false, 1);
+
+    subject.toggleSwitch();
+    checkState(true, 5, 2, true, 3, true, 2);
   });
 
   it('lets observers detach from subjects', () => {
-    subject.detach(observerB);
-    subject.notify();
+    subject.detach(observerA);
+    subject.toggleSwitch();
+    checkState(false, 6, 1, true, 3, false, 3);
 
-    expect(subject.observers.size).to.equal(1);
-    expect(subject.notifications).to.equal(3);
-    expect(observerA.updates).to.equal(2);
-    expect(observerB.updates).to.equal(1);
+    subject.detach(observerB);
+    subject.toggleSwitch();
+    checkState(true, 6, 0, true, 3, false, 3);
   });
 });
